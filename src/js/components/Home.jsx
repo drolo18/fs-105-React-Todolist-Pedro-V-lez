615222d95184e3d2dc802b570a979570ee68b92f
Pedro Vélez <pedrolo.pv@gmail.com>
2025-05-03 12:46:45 +0000

    applied logic

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
-
+import { TaskList } from "./TaskList";
 
 
 const Home = () => {
 
 	const [newtask, setNewTask] = useState('')
 	const [taskList, setTaskList] = useState([])
-	
-	
-	const onChange =(e) => {
+
+
+	const onChange = (e) => {
 		setNewTask(e.target.value)
-		
+
 	}
-	const onKeyDown = (e) =>{
-		if (e.keyCode === 13) {
-		return setTaskList([...taskList, newtask])
+	const onKeyDown = (e) => {
+		if (e.keyCode === 13 && newtask.trim() !== "") {
+			setTaskList([...taskList, newtask])
+			setNewTask("")
 		}
 	}
-	
+	const removeTask = (index) => {
+		const updatedTasks = [
+			...taskList.slice(0, index),
+			...taskList.slice(index + 1),
+		]
+		setTaskList(updatedTasks)
+	}
 
 	return (
 		<div className="text-center">
-		
-			<input placeholder="Escribe tu Tarea"  onChange={onChange} onKeyDown={onKeyDown}/>
-			
-			<div>
-				{taskList.map((task, index) => <p key={index}>{task}</p>) }
+			<h1>Todo List</h1>
+			<br />
+			<div >
+				<input className="text-center border w-50 " placeholder="Escribe tu Tarea" onChange={onChange} onKeyDown={onKeyDown} value={newtask} />
+			</div>
+			<div className="d-flex justify-content-center h-50">
+				<TaskList tasks={taskList} removeTask={removeTask} />
+			</div>
+			<div className="d-flex justify-content-center ">
+				<p className="w-50 text-start last-page" >{taskList.length} item left</p>
 			</div>
 		</div>
 	);
